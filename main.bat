7ad26ecccdede260e988ae1f37a63ef0161ba40b
weaponsforge <forgeweapons@gmail.com>
2019-10-27 04:24:55 +0800

    Added prompt to reset password for user on GitHub, Gitlab and BitBucket.

--- main.bat
+++ main.bat
@@ -4,13 +4,12 @@
 ::----------------------------------------------------------
 
 @echo off
-
 GOTO Main
-
 
 :: Display the main menu
 :Main
   cls
+  echo ----------------------------------------------------------
   echo VIEWING GIT SWITCHER OPTIONS
   echo [1] View git user config
   echo [2] Edit git user config
@@ -30,15 +29,21 @@ EXIT /B 0
 :: Prompt for git config username and email
 :SetUserConfig
   cls
-  echo SET USER CONFIG DETAILS
-  set /p username="Enter git user.name:"
-  git config --global user.name %username%
+  echo ----------------------------------------------------------
+  echo EDIT GIT USER CONFIG DETAILS
+
+  :: Clear the input variables
+  set "gitusername="
+  set "email="
+
+  set /p gitusername="Enter git user.name:"
+  git config --global user.name %gitusername%
 
   set /p email="Enter git user.email:"
   git config --global user.email %email%
 
   echo Updated git user config...
-  GOTO ViewUserConfig
+  GOTO ResetPassword
 EXIT /B 0
 
 
@@ -54,6 +59,79 @@ EXIT /B 0
   echo|set /p=Email:
   git config --get user.email
 
+  set /p choice=Press Enter to continue...
+  GOTO Main
+EXIT /B 0
+
+
+:: Delete the password for the newly-set git user so it will be
+:: prompted on suceeding git operations
+:ResetPassword
+  set gitcredentials=C:\Users\%username%\.gitcredential
+  set newcredentials=C:\Users\%username%\.gitcredentialnew
+  set findstr=@github.com
+  set gitindex=""
+
+  echo Which Git account password would you like to reset?
+  echo [1] Github
+  echo [2] Gitlab
+  echo [3] BitBucket
+  set /p gitindex="Select option:"
+
+  (if %gitindex% EQU 1 (
+    set findstr=@github.com
+  ) else if %gitindex% EQU 2 (
+    set findstr=@gitlab.com
+  ) else if %gitindex% EQU 3 (
+    set findstr=@bitbucket.org
+  ))
+
+  :: Delete temporary file
+  if exist %newcredentials% (
+    del /f %newcredentials%
+  )
+
+  if exist %gitcredentials% (
+    (for /f "tokens=*" %%x in (%gitcredentials%) do (
+      @echo.%%x | findstr /C:%findstr%>nul && (
+        Echo found
+
+        :: Copy all git credentials to temp file  excluding the user to reset
+        type %gitcredentials% | findstr /v %findstr% >> %newcredentials%
+
+        :: Overwrite current git credentials to delete (reset) the new git config user's password
+        type %newcredentials% > %gitcredentials%
+        GOTO ExitResetPassword
+      )
+
+      GOTO ExitResetPassword
+    ))
+  ) else (
+    echo %gitcredentials% was not found.
+    echo Make sure your windows User path is correct and try again.
+    GOTO ProcessError
+  )
+EXIT /B 0
+
+
+:ExitResetPassword
+  :: Delete temporary git credentials file
+  if exist %newcredentials% (
+    del /f %newcredentials% && (
+      set /p choice=New git user's password has been reset...
+      GOTO ViewUserConfig
+    ) || (
+      set /p choice=Failed deleting temporary file.
+      GOTO ProcessError
+    )
+  ) else (
+    echo Password for user %gitusername% on %findstr% was not found.
+    GOTO ProcessError
+  )
+EXIT /B 0
+
+
+:ProcessError
   set /p choice=Press Enter to continue...
   GOTO Main
 EXIT /B 0
